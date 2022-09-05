54a1861fd2da8e6a9e58d0744965fe325d1f4e8c
salomao-mafalda <salomaomachadon@gmail.com>
2022-09-05 13:31:25 -0500

    Update Types of Features

--- 9-Real_Time_Detect/detectCamObj.m
+++ 9-Real_Time_Detect/detectCamObj.m
@@ -3,13 +3,19 @@ camObj = webcam(1);
 TargetImage = imread("target.png");
 TargetImage = rgb2gray(TargetImage);
 %TargetImage =  adapthisteq(TargetImage);
-
+fps = 0;
+avgfps = [];
 while(1)
+    
     SceneImage1 = snapshot(camObj);
+    tic;
     SceneImage = rgb2gray(SceneImage1);
     %SceneImage = adapthisteq(SceneImage);
     [matchedScenePoints, matchedBoxPoints] = featurextract(SceneImage,TargetImage);
     len = length(matchedScenePoints);
+    newt = toc;
+    fps = .9*fps + .1*(1/newt);
+    avgfps = [avgfps, fps];
     if len>20
         disp('obj detected');
         bbox = [1, 1; ...
@@ -21,11 +27,11 @@ while(1)
         [tform, inlierBoxPoints, inlierScenePoints] = ...
             estimateGeometricTransform(matchedBoxPoints,matchedScenePoints, 'affine');
         newBoxPolign = transformPointsForward(tform, bbox)
-        d='object recognized'
+        d=sprintf("object recognized, FPS %2.2f", fps);
         currFrame = SceneImage;
         position = [321 430];
         box_color = {'yellow'};
-        RGB = insertText(currFrame, position, d, 'FontSize',22, 'BoxColor',box_color);
+        RGB = insertText(currFrame, position, sprintf('FPS %2.2f', fps), 'FontSize',22, 'BoxColor',box_color);
         imshow(RGB);
         hold on;
         line(newBoxPolign(:,1), newBoxPolign(:,2), 'Color', 'y' );
@@ -36,7 +42,7 @@ while(1)
         currentFrame = SceneImage;
         position = [321 430];
         box_color = {'yellow'};
-        RGB = insertText(currentFrame, position, d, 'FontSize',22, 'BoxColor',box_color);
+        RGB = insertText(currentFrame, position, sprintf('FPS %2.2f', fps), 'FontSize',22, 'BoxColor',box_color);
         imshow(RGB);
         hold on;
     end
